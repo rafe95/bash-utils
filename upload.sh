252c94a343b068e44f6dd973e41ba89392db3f29
Rafael Estevez <rafe.estevez@gmail.com>
2022-01-16 12:29:22 -0500

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-#description :This script will allow to upload files to a nextcloud server via webdav.
-#author		 :rafe95
+#description    :This script will allow to upload files to a nextcloud server via webdav.
+#usage          :sh upload.sh file(s)
+#notes          :Install duck.sh to use this script.
+#author         :rafe95
+#=========================================================================
 
 printf 'Username: '
 read -r username
